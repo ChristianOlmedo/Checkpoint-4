41eedf9eb89a6209fcdb2ac4cb7ef015f8a23678
ChristianOlmedo <christian.olmedo0210@gmail.com>
2021-07-21 19:38:32 +0200

    connexion, inscription

---
+++ src/Controller/EventController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+    /**
+     * @Route("/évènements", name="event")
+     */
+    public function index(): Response
+    {
+        return $this->render('event/event.html.twig');
+    }
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+
+        $user->setEmail('user@monsite.com');
+
+        $user->setFirstname('user');
+
+        $user->setLastname('user');
+
+        $user->setNickname('user');
+
+        $user->setRoles(['ROLE_USER']);
+
+        $user->setPassword($this->passwordEncoder->encodePassword(
+
+            $user,
+
+            'user'
+
+        ));
+
+
+        $manager->persist($user);
+
+        // Création d’un utilisateur de type “administrateur”
+
+        $admin = new User();
+
+        $admin->setEmail('admin@monsite.com');
+
+        $admin->setFirstname('admin');
+
+        $admin->setLastname('admin');
+
+        $admin->setNickname('admin');
+
+        $admin->setRoles(['ROLE_ADMIN']);
+
+        $admin->setPassword($this->passwordEncoder->encodePassword(
+
+            $admin,
+
+            'admin'
+
+        ));
+
+
+        $manager->persist($admin);
+
+
+        // Sauvegarde des 2 nouveaux utilisateurs :
+
+        $manager->flush();
+    }
+}
